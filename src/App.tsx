8a8cc7976acd2b7f109e7ef25f50b9bed0a00082
Leonardo Soares <leo.soares64@outlook.com>
2020-06-04 01:33:35 -0300

    contruçao do html do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home'
+import Routes from './routes';
 
-// JSX: sintaxe de XML dentro do JavaScript
 
 function App() {
   return (
-      <Home /> 
+    <Routes />
   );
 }
 
